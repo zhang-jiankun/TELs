352db24469f41c79d1cb3356b529f16129e37adb
zhang-jiankun <zhangjk21@gmail.com>
2023-09-13 10:39:26 +0800

    add hic function

--- data_processing.py
+++ data_processing.py
@@ -1,4 +1,4 @@
-def avg_matrix_score(matrix, norm=False, cnorm=False):
+def read_matrix_score(matrix, norm=False, cnorm=False):
     """ Read `deeptools computeMatrix` output """
     df = pd.read_csv(matrix, sep='\t', comment='@', header=None)
     df = df.drop(columns=list(range(6)))
@@ -18,3 +18,16 @@ def read_bw_score(filename, sample=None):
     df = df.sort_values(['signal'], ascending=False)
     
     return df
+
+
+def filter_insulation_boundaries(insul_path, window=100000, threshold=0.2):
+    """ 
+        Filter weak insulation boundaries.
+        See https://github.com/open2c/cooltools/issues/64.
+    """
+    insul_df = pd.read_csv(insul_path, sep='\t')
+    insul_df = insul_df[(insul_df['is_bad_bin']==False) & (insul_df[f'is_boundary_{window}']==True)]
+    insul_df = insul_df[insul_df[f'boundary_strength_{window}']>threshold] 
+    insul_df = insul_df[['chrom', 'start', 'end']]
+    return inful_df
+
